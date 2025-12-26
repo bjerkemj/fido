36a97eebd98046d3b54deb232d25ef7c60b71bc4
Johan <bjerkem.j@gmail.com>
2025-12-26 17:23:30 +1000

    first commit

---
+++ app/dogs/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const DogDetails = () => {
+    return (
+        <View>
+            <Text>DogDetails</Text>
+        </View>
+    )
+}
+
+export default DogDetails;
